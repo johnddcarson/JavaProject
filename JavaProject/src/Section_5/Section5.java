855382ea085893991221b9076f0e0d822d37d44f
JohnCarson <JohnCarson@192.168.1.173>
2023-10-11 15:45:34 +0100

    Learning Methods

--- JavaProject/src/Section_5/Section5.java
+++ JavaProject/src/Section_5/Section5.java
@@ -4,8 +4,54 @@ public class Section5 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello John");
+		double kilometers = (100 * 1.609344);
 
+		int highScore = 50;
+
+		if (highScore > 25) {
+			highScore = 500 + highScore;
+
+			System.out.println(highScore);
+		}
+
+		int health = 100;
+
+		if ((health > 25) && (highScore < 1000)) {
+			highScore = 555;
+
+			System.out.println(highScore);
+			highScore--;
+			System.out.println(highScore);
+
+			for (int i = highScore; i <= 560; i++) {
+				System.out.println(highScore++);
+			}
+		}
+
+		boolean gameOver = true;
+		int score = 800;
+		int levelCompleted = 5;
+		int bonus = 100;
+
+		int finalScore = 0;
+
+		if (gameOver) {
+			finalScore = calculateScore(levelCompleted, bonus);
+			System.out.println("Your final score was " + finalScore);
+			finalScore = calculateScore(levelCompleted, bonus, 500);
+			System.out.println("Your final score was " + finalScore);
+		}
+
+	}
+
+	public static int calculateScore(int levelCompleted, int bonus) {
+
+		return levelCompleted * bonus;
+	}
+
+	public static int calculateScore(int levelCompleted, int bonus, int ExtraBonus) {
+
+		return (levelCompleted * bonus) + ExtraBonus;
 	}
 
 }
