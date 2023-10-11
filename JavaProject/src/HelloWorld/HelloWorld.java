415799116af4d86499e971b45a3ff327326ac318
JohnCarson <JohnCarson@192.168.1.173>
2023-10-11 10:00:10 +0100

    Hello World

---
+++ JavaProject/src/HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World, John");
+
+		boolean isAlien = true;
+
+		if (isAlien) {
+			System.out.println("It is not an Alien");
+		} else {
+			System.out.println("It is an Alien");
+		}
+		
+		int topScore = 100;
+		
+		if(topScore != 100) {
+			System.out.println("You got the high score");
+		}
+
+	}
+
+}
