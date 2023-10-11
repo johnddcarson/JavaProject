5fb5c114a4c152d81f8a1a58f013315b2643de1a
JohnCarson <JohnCarson@192.168.1.173>
2023-10-11 12:21:44 +0100

    Var change

--- JavaProject/src/Section_4/Section4.java
+++ JavaProject/src/Section_4/Section4.java
@@ -4,8 +4,8 @@ public class Section4 {
 
 	public static void main(String[] args) {
 
-		double doubleVar1 = 20.00;
-		double doubleVar2 = 80.00;
+		double doubleVar1 = 10.00;
+		double doubleVar2 = 90.00;
 		double calDoubleVar = (doubleVar1 + doubleVar2) * 100.00;
 		double remainder = calDoubleVar % 40;
 		boolean isremainder = remainder == 0.00 ? true : false;
